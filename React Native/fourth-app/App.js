ab1cdeb44db3711428ce9db9eab8c2171f73a375
Howida Abd El-Halim <100088433+howidaabdelhalim@users.noreply.github.com>
2024-09-26 14:06:08 +0300

    Update App.js

--- React Native/fourth-app/App.js
+++ React Native/fourth-app/App.js
@@ -1,3 +1,4 @@
+//Users CRUD using FireBase
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
